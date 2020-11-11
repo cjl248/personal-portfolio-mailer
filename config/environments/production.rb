b6618847e1514d4848bd254bde8ecf6d63c3f7f3
github cjl248 <github cjl248@cornell.edu>
2020-11-11 16:05:13 -0500

    Set up SMTP with gmail. Invoking FormsMailer within MailsController render email and attempt to send - running into error: '#<Net::SMTPAuthenticationError: 535-5.7.8 Username and Password not accepted.>'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,26 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  # Added the following to set up mailer with GMAIL:
+
+    # Set method to SMTP
+    config.action_mailer.delivery_method = :smtp
+
+    # => TODO: Set host to backend url once deploying
+    _host = '' # => # TODO
+    config.action_mailer.default_url_options = { host: _host}
+
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV["FROM_EMAIL"],
+      :password             => ENV["EMAIL_PASSWORD"],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+    }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- test/mailers/previews/form_mailer_preview.rb
+++ test/mailers/previews/form_mailer_preview.rb
@@ -1,4 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/form_mailer
 class FormMailerPreview < ActionMailer::Preview
 
+  def new_form_email
+    @mail = FormMail.new(
+      name: "Chris Liendo",
+      email: "chris@gmail.com",
+      subject: "May have a new job for you!",
+      text: "Hi Chris - I took a look at your resume and projects and I think you would be a good fit for a jr. fullstack developer role at Google. Let me know if interested: 646-338-0622"
+    )
+    FormMailer.with(mail: @mail).new_form_email
+  end
+
 end
