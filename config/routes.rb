cfdef8b5bee7df7b407dc8daf9f2d43591490cc0
github cjl248 <github cjl248@cornell.edu>
2020-11-12 11:14:01 -0500

    removed root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'mails#index'
-  
+
   get '/form_mail', to: 'mails#index'
   post '/form_mail', to: 'mails#create'
 
