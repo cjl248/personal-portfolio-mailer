c0a3f7c1101ed032f8576680102ad4134d5f55f1
github cjl248 <github cjl248@cornell.edu>
2020-11-12 10:59:46 -0500

    Added root route and rolled back database.yml changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'mails#index'
+  
   get '/form_mail', to: 'mails#index'
   post '/form_mail', to: 'mails#create'
-  
+
 end
